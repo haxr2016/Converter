f9803b9421796fcc013b97030a4e5a1ff3c115f8
Hemanth Balakrishna <hbalakrishna@hawk.iit.edu>
2017-07-14 14:17:29 +0530

    history & scroll commit

--- app/src/main/java/com/example/hemanth/converter/MainActivity.java
+++ app/src/main/java/com/example/hemanth/converter/MainActivity.java
@@ -4,9 +4,18 @@ import android.content.Intent;
 import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static android.R.id.message;
 
@@ -15,15 +24,23 @@ public class MainActivity extends AppCompatActivity {
     private EditText editText1;
     private RadioGroup button_group;
     private EditText editText;
+    private TextView history;
+    private Button button;
+    String[] ListElements = new String[]{"a", "b"};
     int selectedId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.converter_layout);
+        setContentView(R.layout.activity_main);
 
         editText = (EditText) findViewById(R.id.editText2);
         editText1 = (EditText) findViewById(R.id.editText);
+        history = (TextView) findViewById(R.id.history);
+        history.setText("");
+        history.setMovementMethod(new ScrollingMovementMethod());
+        button = (Button) findViewById(R.id.button);
+
         editText1.setKeyListener(null);
         button_group = (RadioGroup) findViewById(R.id.RG);
 
@@ -71,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
                 n1 = f2c(n1);
                 message1 = n1+"";
                 editText1.setText(message1);
+                String str = "C to F: ";
+                createHistory(str);
+
                 break;
 
 
@@ -80,10 +100,48 @@ public class MainActivity extends AppCompatActivity {
                 n = c2f(n);
                 message = n+"";
                 editText1.setText(message);
+                String str1 = "F to C: ";
+                createHistory(str1);
+
+
                 break;
         }
 
     }
+
+    private void createHistory(final String str1) {
+        button = (Button) findViewById(R.id.button);
+
+
+        String newText =  editText.getText().toString();
+        String newText2 = editText1.getText().toString();
+        String message = str1 + newText +" -> "+ newText2;
+        String historyText = history.getText().toString();
+        history.setText(message + "\n" + historyText);
+//        numText.setText("");
+
+//        final List<String> ListElementsArrayList = new ArrayList<String>(Arrays.asList(ListElements));
+//
+//
+//        final ArrayAdapter<String> adapter = new ArrayAdapter<String>
+//                (MainActivity.this, R.layout.activity_main, ListElementsArrayList);
+//
+//        listView.setAdapter(adapter);
+//
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//
+//                ListElementsArrayList.add(str1 + string1.getText().toString() + string2.getText().toString());
+//
+//                adapter.notifyDataSetChanged();
+//
+//    }
+//
+//    });
+    }
+
 
     private double c2f(double n) {
         return (n * (9.0 / 5.0) + 32);
